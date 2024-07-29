2a7d334034419d8065764f9c79534d258473540d
MONKE-source <130353466+MONKE-source@users.noreply.github.com>
2024-07-29 21:26:54 +0800

    Changed 3D libary to 3Js

--- src/screens/inc.jsx
+++ src/screens/inc.jsx
@@ -1,4 +1,3 @@
-import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
 import { useLocation } from "react-router-dom";
 import React, { useEffect } from "react";
 
@@ -16,16 +15,7 @@ function IncHq() {
     console.error("Error loading image: ", event);
   };
 
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactPhotoSphereViewer
-        src={image || "example.jpeg"}
-        height={"100vh"}
-        width={"100%"}
-        onError={handleImageError}
-      />
-    </div>
-  );
+  return <div style={{ width: "100vw", height: "100vh" }}></div>;
 }
 
 export default IncHq;
